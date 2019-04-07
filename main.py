259f71cb8366430dca5032c56533f69be800e6c3
DobrijZmej <zmej@vps-27277.vps-default-host.net>
2019-04-07 15:57:14 +0300

    init

---
+++ main.py
@@ -0,0 +1,90 @@
+import random
+import threading
+import traceback
+from math import sqrt
+
+import discord
+import asyncio
+import json
+
+from pathlib import Path
+import urllib.request
+
+import uuid
+
+import os
+
+
+import requests
+import lxml.html, lxml.etree
+
+import mysql_data
+
+
+
+client = discord.Client()
+
+def get_value(dict, key):
+    if(key in dict):
+        return dict[key]
+    return ''
+
+def get_channel_object(name):
+    #print("=================")
+    for server in client.servers:
+        for channel in server.channels:
+            #print(channel.name+" "+channel.id)
+            if(channel.name == name):
+                return channel
+
+
+@client.event
+async def on_ready():
+    print('Loggid in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('----------')
+    discord.Status
+    #bot_game = discord.Game(name="Єдина Україна", url="http://ed.dobrijzmej.org", type=0)
+    #await client.change_presence(game=bot_game)
+
+@client.event
+async def on_message(message):
+
+    prefix = '!'
+
+    if message.content.startswith(prefix+"я "):
+        await on_safe_cmdr_name(message)
+
+""" ================== сохранить имя командира ============================="""
+async def on_safe_cmdr_name(message):
+    """in_server, in_cmdr_mention, in_cmdr_name"""
+    new_name = ""
+    message_args = message.content.split()
+    for n in message_args[1:]:
+        if(new_name!=''):
+            new_name += " "
+        new_name += n
+    print("new_name="+new_name)
+    
+    site_url = "https://uca.co.ua/pilot/registry/"
+    pilots_count = mysql_data.check_pilot(new_name);
+    discords_count = mysql_data.check_discord_id(message.author.id);
+    if(pilots_count == 0 and discords_count == 0):
+        user_uuid = uuid.uuid4().hex
+        mysql_data.registry_pilot(new_name, message.author.id, user_uuid)
+        await client.send_message(message.author, "Доречі, я підготував для тебе особистий кабінет на сторінці нашої спільноти, можеш зареєструватися щє й там: "+site_url+user_uuid)
+        return
+    if(pilots_count!=0):
+        user_uuid = mysql_data.get_uid_by_pilot(new_name)
+        await client.send_message(message.author, "Доречі, я все щє очікую, коли ти зарєструешся у своєму кабінеті на нашому сайті: "+site_url+user_uuid)
+        return
+    if(discords_count!=0):
+        user_uuid = mysql_data.get_uid_by_pilot(new_name)
+        await client.send_message(message.author, "Доречі, я все щє очікую, коли ти зарєструешся у своєму кабінеті на нашому сайті: "+site_url+user_uuid)
+
+
+client.run('MjkyNzA1OTA2NzIwNzY4MDAx.DkhoUw.FaFJQmelVNJze-uVwkLt12RQh6U')
+#await client.connect()
+#await client.login(token='MjkyNzA1OTA2NzIwNzY4MDAx.C7wbYQ.s3Wu5gFopQn6zP05Mn-O1WeTpdg', bot=True)
+
---
+++ mysql_data.py
@@ -0,0 +1,98 @@
+import MySQLdb
+
+#*******************************************************************************
+def check_pilot(in_ed_name):
+
+#    cnx = mysql.connector.connect(user='uca', password='7014258',
+#                                  host='127.0.0.1',
+#                                  database='uca')
+
+    cnx = MySQLdb.connect(user='uca', password='7014258',
+                                  host='127.0.0.1',
+                                  database='uca')
+                                  
+    cursor = cnx.cursor()
+    query = """select count(1) from discord_registers where ed_name = %(ed_name)s and user_id is null"""
+    
+    cursor.execute(query, {'ed_name': in_ed_name})
+
+    for(count) in cursor:
+       print(count[0])
+
+    cursor.close()
+    cnx.close()
+    
+    return count[0]
+
+#*******************************************************************************
+def check_discord_id(in_discord_id):
+    cnx = MySQLdb.connect(user='uca', password='7014258',
+                                  host='127.0.0.1',
+                                  database='uca')
+#    cnx = mysql.connector.connect(user='uca', password='7014258',
+#                                  host='127.0.0.1',
+#                                  database='uca')
+                                  
+    cursor = cnx.cursor()
+    query = """select count(1) from discord_registers where discord_id = %(discord_id)s"""
+    
+    cursor.execute(query, {'discord_id': in_discord_id})
+
+    for(count) in cursor:
+       print(count[0])
+
+    cursor.close()
+    cnx.close()
+    
+    return count[0]
+
+#*******************************************************************************
+def get_uid_by_pilot(in_ed_name):
+    cnx = MySQLdb.connect(user='uca', password='7014258',
+                                  host='127.0.0.1',
+                                  database='uca')
+#    cnx = mysql.connector.connect(user='uca', password='7014258',
+#                                  host='127.0.0.1',
+#                                  database='uca')
+                                  
+    cursor = cnx.cursor()
+    query = """select registry_uid from discord_registers where ed_name = %(ed_name)s limit 1"""
+    
+    cursor.execute(query, {'ed_name': in_ed_name})
+
+    for(registry_uid) in cursor:
+       print(registry_uid[0])
+
+    cursor.close()
+    cnx.close()
+    
+    return registry_uid[0]
+
+#*******************************************************************************
+def registry_pilot(in_ed_name, in_discord_id, in_uid):
+    cnx = MySQLdb.connect(user='uca', password='7014258',
+                                  host='127.0.0.1',
+                                  database='uca')
+#    cnx = mysql.connector.connect(user='uca', password='7014258',
+#                                  host='127.0.0.1',
+#                                  database='uca')
+                                  
+    cursor = cnx.cursor()
+    
+    add_user = ("INSERT INTO discord_registers "
+                  "(ed_name, discord_id, registry_uid) "
+                  "VALUES (%(ed_name)s, %(discord_id)s, %(registry_uid)s)")
+                  
+    data_user = {
+      'ed_name': in_ed_name,
+      'discord_id': in_discord_id,
+      'registry_uid': in_uid,
+    }
+    
+    cursor.execute(add_user, data_user)
+    
+    cnx.commit()
+    
+    cursor.close()
+    cnx.close()
+    
